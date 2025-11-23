c32901647c8d8a2bbb29e6f6f6686b31bee9bcf4
thangvh2004 <thangvh2004@gmail.com>
2025-11-23 20:53:59 +0700

    Comment firebase code

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'firebase_options.dart';
 import 'package:ecmobile/theme/app_colors.dart';
 import 'package:ecmobile/layouts/main_layout.dart';
 // 1. IMPORT FILE MỚI
-import 'package:ecmobile/utils/seed_customer.dart';
+//import 'package:ecmobile/utils/seed_customer.dart';
 // 1. QUAN TRỌNG: Import file chứa hàm nạp dữ liệu bạn vừa tạo
 // (Đảm bảo bạn đã tạo file lib/utils/seed_data.dart và dán code tôi gửi ở tin nhắn trước)
-import 'package:ecmobile/utils/seed_data.dart';
+//import 'package:ecmobile/utils/seed_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,10 +18,10 @@ void main() async {
 
   // 2. GỌI HÀM NẠP DỮ LIỆU TẠI ĐÂY
   // Khi chạy App, nó sẽ thực thi hàm này để bắn dữ liệu lên Firebase
-  print("--- BẮT ĐẦU NẠP DỮ LIỆU ---");
-  await seedRealData();
-  await seedCustomerSystem();
-  print("--- KẾT THÚC NẠP DỮ LIỆU ---");
+  //print("--- BẮT ĐẦU NẠP DỮ LIỆU ---");
+  //await seedRealData();
+  //await seedCustomerSystem();
+  //print("--- KẾT THÚC NẠP DỮ LIỆU ---");
 
   runApp(const MyApp());
 }
